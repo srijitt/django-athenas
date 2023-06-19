6b0dee82189c3db3a7b03162e83e5362b4de888e
srijitt <srijit.chakraborty154@gmail.com>
2023-06-19 21:20:25 +0530

    back to normal

--- athenas/settings.py
+++ athenas/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = 'django-insecure-x4^h4nt^j(nxn*_7@a99v3k8xze5q43j_)@lsn(%n2u+j4ve2-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app',
-                 '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
